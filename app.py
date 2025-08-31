2ec51b3a8df700ca0f5bd54708cdceda355e7e4a
Rohit Wankar <rohitwankar88@gmail.com>
2025-08-31 16:09:08 +0530

    RWW

--- app.py
+++ app.py
@@ -392,4 +392,4 @@ def plot_ph_diagram():
 # Run Flask App
 # ======================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
